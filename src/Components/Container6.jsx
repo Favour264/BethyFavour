b7e5518d22c5ff8d2160e342a39ed0f530a93351
Favour264 <adamsfavour264@gmail.com>
2024-06-12 16:12:42 -0700

    space

--- src/Components/Container6.jsx
+++ src/Components/Container6.jsx
@@ -12,7 +12,7 @@ export const Container6 = () =>{
   </div>
   <div className="flex flex-col sm:flex-row float-right gap-3 sm:gap-5 ">
     <div className="flex-col sm:flex-row">
-        <div className="w-80 bg-white border-2 border-gray-400 ml-10 lg:ml-0 md:ml-0 sm:ml-0  mr-0 sm:mr-20 mb-5 sm:mb-20">
+        <div className="w-80 bg-white border-2 border-gray-400 ml-2 lg:ml-0 md:ml-0 sm:ml-0  mr-0 sm:mr-20 mb-5 sm:mb-20">
             <img  className="w-80  object-cover" src={photo1} alt="Picture" />
             <div className="p-2">
                 <h1 className='text-2xl font-bold text-black mb-5'>AI IN MANUFACTURING</h1>
@@ -21,7 +21,7 @@ export const Container6 = () =>{
             </div>
         </div>
 
-        <div className="w-80 bg-white border-2 border-gray-400 ml-10 lg:ml-0 md:ml-0 sm:ml-0 mb-10 sm:mb-20">
+        <div className="w-80 bg-white border-2 border-gray-400 ml-2 lg:ml-0 md:ml-0 sm:ml-0 mb-10 sm:mb-20">
             <img  className="w-80 object-cover" src={photo1} alt="Picture" />
             <div className="p-2">
                 <h1 className='text-2xl font-bold text-black mb-5'>Green Advancements</h1>
@@ -31,7 +31,7 @@ export const Container6 = () =>{
         </div>
     </div>
     <div className="flex-col sm:flex-row">
-        <div className="w-80 bg-white border-2 border-gray-400 ml-10 lg:ml-0 md:ml-0 sm:ml-0 sm:mr-20 mr-0  sm:mb-20 mb-10 ">
+        <div className="w-80 bg-white border-2 border-gray-400 ml-2 lg:ml-0 md:ml-0 sm:ml-0 sm:mr-20 mr-0  sm:mb-20 mb-10 ">
             <img  className="w-80 object-cover" src={photo1} alt="Picture" />
             <div className="p-2">
                 <h1 className='text-2xl font-bold text-black mb-5'>Blockchain & Security</h1>
@@ -39,7 +39,7 @@ export const Container6 = () =>{
                 <p className="">Explore the transformative power of Artificial Intelligence in manufacturing, as we delve into cutting-edge applications and how it's reshaping traditional processes to boost efficiency, reduce costs, and accelerate the industry into the future.</p>
             </div>
         </div>
-        <div className="w-80 bg-white border-2 border-gray-400 ml-10 lg:ml-0 md:ml-0 sm:ml-0 mb-5">
+        <div className="w-80 bg-white border-2 border-gray-400 ml-2 lg:ml-0 md:ml-0 sm:ml-0 mb-5">
             <img  className="w-80 object-cover" src={photo1} alt="Picture" />
             <div className="p-2">
                 <h1 className='text-2xl font-bold text-black mb-5'>Quantum Computing</h1>
